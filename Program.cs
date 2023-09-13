fae7a2a11d4663dbb79098b28ef3d51a70742fb0
karthik475 <82768678+karthik475@users.noreply.github.com>
2023-09-13 10:52:54 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace FunctionTutorial
+{
+    class Program
+    {
+        public static void printnumber(int number)
+        {
+            Console.WriteLine(number);
+        }
+        public static int squared(int x)
+        {
+            int y = x * x;
+            return y;
+        }
+        public static Boolean isbigger(int frst,int sec)
+        {
+            Boolean temp = frst < sec;
+            return temp;
+        }
+        
+        static void Main(string[] args)
+        {
+            int temp = 0;
+            Console.WriteLine(squared(5));
+            printnumber(temp);
+            Console.WriteLine(isbigger(100, 20));
+        }
+    }
+}
