ca0bd5f73aadb799584f6b84bde33a47c0bf6a83
karthik475 <82768678+karthik475@users.noreply.github.com>
2023-09-13 10:14:22 +0530

    Create VariablesTutorial.cs

---
+++ VariablesTutorial.cs
@@ -0,0 +1,30 @@
+using System;
+namespace VariablesTutorial
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            int num;
+            num = 0;
+            Console.WriteLine(num);
+            num = 10;
+            Console.WriteLine(num);
+            num = 2 + 2;
+            Console.WriteLine(num);
+            double dnum;
+            dnum = 1.56;
+            int n = 2;
+            Console.WriteLine(n);
+            Console.WriteLine(dnum);
+            char mychar='A';
+            Console.WriteLine(mychar);
+            string mystr = "Karthik";
+            Console.WriteLine(mystr);
+            int nwvar = num - n;
+            Console.WriteLine(nwvar);
+            bool temp=true;
+            Console.WriteLine(temp);
+        }
+    }
+}
