9fafc844f16456844021cb4bab38fa8dfcd57771
karthik475 <82768678+karthik475@users.noreply.github.com>
2023-09-13 10:09:04 +0530

    Create Introduction_c_sharp.cs

---
+++ Introduction_c_sharp.cs
@@ -0,0 +1,12 @@
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello World!");
+        Main1();
+    }
+    static void Main1()
+    {
+        Console.WriteLine("This is 2023");
+    }
+}
